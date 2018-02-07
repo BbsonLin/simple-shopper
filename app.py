7d749324a5de356a694792a7fede9ce74625f6c7
WalkerHo <uitk23009@gmail.com>
2018-02-07 14:24:17 +0800

    Add default value

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ products = {
     '/cat1': [
         {'id': 1, 'image': 'https://images.unsplash.com/photo-1453831362806-3d5577f014a4', 'name': '商品一', 'description': '一個不錯的商品'},
         {'id': 2, 'image': 'https://images.unsplash.com/photo-1473093226795-af9932fe5856', 'name': '商品二', 'description': '另一個不錯的商品'},
-        {'id': 3, 'image': '', 'name': '商品一', 'description': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora quam quia facilis dolor aliquid veritatis.'},
+        {'id': 3, 'image': 'https://images.unsplash.com/photo-1473093226795-af9932fe5856', 'name': '商品一', 'description': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora quam quia facilis dolor aliquid veritatis.'},
         {'id': 4, 'image': 'https://images.unsplash.com/photo-1453831362806-3d5577f014a4', 'name': '商品二', 'description': '另一個不錯的商品'},
         {'id': 5, 'image': 'https://images.unsplash.com/photo-1473093226795-af9932fe5856', 'name': '商品一', 'description': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora quam quia facilis dolor aliquid veritatis.'}
     ],
