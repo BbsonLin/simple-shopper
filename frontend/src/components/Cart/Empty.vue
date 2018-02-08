cbe44344b4443e15e0cf5777139097367a65539c
WalkerHo <uitk23009@gmail.com>
2018-02-08 20:32:54 +0800

    Setting cart empty page

---
+++ frontend/src/components/Cart/Empty.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="empty">
+  <h2 class="empty-message">
+    Your cart is empty
+  </h2>
+  <button class="btn btn-primary" @click="$router.push('/')">Back to Shop</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'empty'
+}
+</script>
+
+<style lang="scss">
+$empty-background: #eee;
+.empty {
+  width: 80%;
+  height: 60%;
+  background-color: $empty-background;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
---
+++ frontend/src/components/Cart/index.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="cart">
+  <h1 class="cart-title">Cart</h1>
+  <empty v-if="!totalNumber"></empty>
+</div>
+</template>
+
+<script>
+import Empty from './Empty'
+import { mapGetters } from 'vuex'
+export default {
+  name: 'cart',
+  components: {
+    Empty
+  },
+  computed: {
+    ...mapGetters({
+      totalNumber: 'getTotalNumber'
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+.cart {
+  padding: 10px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
