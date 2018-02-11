c357abb8be6acbaa8d3c7939803c7c53d2fe1204
WalkerHo <uitk23009@gmail.com>
2018-02-11 17:02:32 +0800

    Add stepone page

--- frontend/src/components/Cart/Stepone.vue
+++ frontend/src/components/Cart/Stepone.vue
@@ -1,14 +1,41 @@
 <template>
-<div class="cart">
-  <h1 class="cart-title">Cart</h1>
+<div class="stepone">
+  <table class="table">
+    <tbody>
+      <tr class="table-active">
+        <td>商品名稱</td>
+        <td>數量</td>
+        <td>單價</td>
+        <td>小計</td>
+        <td>操作</td>
+      </tr>
+      <tr v-for="product in cartProducts">
+        <td>{{ product.name }}</td>
+        <td>{{ product.number }}</td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.price * product.number }}</td>
+        <td><i class="material-icons">clear</i></td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'cart'
+  name: 'stepone',
+  computed: {
+    ...mapGetters({
+      cartProducts: 'getCartProducts',
+      totalNumber: 'getTotalNumber'
+    })
+  }
 }
 </script>
 
 <style lang="scss">
+.stepone {
+  width: 80%;
+}
 </style>
--- frontend/src/components/Cart/index.vue
+++ frontend/src/components/Cart/index.vue
@@ -1,25 +1,48 @@
 <template>
 <div class="cart">
   <h1 class="cart-title">Cart</h1>
-  {{ totalNumber }}
   <empty v-if="!totalNumber"></empty>
-  <!-- <stepone v&#45;if="step===1"></stepone> -->
-  <button class="btn btn-primary check-button" v-if="totalNumber">下一步</button>
+  <stepone v-if="totalNumber && step===1"></stepone>
+  <div class="check-button">
+    <button class="btn btn-primary" v-if="totalNumber" @click="cartEvent">{{ button.content }}</button>
+  </div>
 </div>
 </template>
 
 <script>
 import Empty from './Empty'
+import Stepone from './Stepone'
 import { mapGetters } from 'vuex'
 export default {
   name: 'cart',
   components: {
-    Empty
+    Empty,
+    Stepone
+  },
+  data () {
+    return {
+      step: 1,
+      button: {
+        content: '結帳'
+      }
+    }
   },
   computed: {
     ...mapGetters({
       totalNumber: 'getTotalNumber'
     })
+  },
+  methods: {
+    cartEvent () {
+      switch (this.step) {
+        case 1:
+          this.step++
+          this.button.content = '完成'
+        case 2:
+          this.step = 1
+          this.button.content = '結帳'
+      }
+    }
   }
 }
 </script>
@@ -34,6 +57,8 @@ export default {
 }
 
 .check-button {
-  margin-left: auto;
+  width: 80%;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
