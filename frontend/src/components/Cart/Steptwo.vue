0b2eefc06bbeafae6c2911fe60214c0aeab46762
WalkerHo <uitk23009@gmail.com>
2018-02-12 11:04:30 +0800

    Add steptwo vue file

---
+++ frontend/src/components/Cart/Steptwo.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="steptwo">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'steptwo',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
