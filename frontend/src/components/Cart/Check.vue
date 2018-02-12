fcedcf10eaaef1dd3b5ec4e3d19e3f42c226b181
WalkerHo <uitk23009@gmail.com>
2018-02-12 14:04:05 +0800

    Steptwo component sets check method button

--- frontend/src/components/Cart/Check.vue
+++ frontend/src/components/Cart/Check.vue
@@ -1,13 +1,21 @@
 <template>
 <div class="check">
   <div class="form-group">
-    <label for="storeSelect">取貨地點</label>
+    <label>取貨地點</label>
     <multiselect id="storeSelect" v-model="selectedStore" :options="stores" label="label" :searchable="false" :allow-empty="false" :show-labels="false"></multiselect>
+  </div>
+  <div class="form-group">
+    <label>付款方式</label>
+    <button class="btn" :class="buttonType" @click="checkMethod('wechat')">微信支付</button>
+  </div>
+  <div class="summary">
+    <button class="btn btn-primary" :disabled="disabled" @click="addStep">完成</button>
   </div>
 </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   name: 'check',
   data () {
@@ -22,7 +30,20 @@ export default {
           value: 1,
           label: '上海'
         }
-      ]
+      ],
+      buttonType: 'btn-outline-secondary',
+      disabled: true
+    }
+  },
+  methods: {
+    ...mapActions(['addStep']),
+    checkMethod (type) {
+      switch (type) {
+        case 'wechat':
+          this.buttonType = 'btn-outline-success'
+          this.disabled = false
+          break
+      }
     }
   }
 }
@@ -41,6 +62,15 @@ export default {
       width: 100px;
       margin: auto 0;
     }
+
+    .multiselect {
+      width: calc(100% - 100px);
+    }
+  }
+
+  .summary {
+    display: flex;
+    justify-content: flex-end;
   }
 }
 </style>
