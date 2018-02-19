54f1f826073128984e50c7876a5a8c2679e600d5
WalkerHo <uitk23009@gmail.com>
2018-02-19 20:23:48 +0800

    Add axios api setting

---
+++ frontend/src/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import * as url from './urlConfig'
+
+//Product
+export const requestProduct = {
+  List: params => { return axios.get(url.product, { params: params }).then(res => res.data) }
+}
+
+//Store
+export const requestStore = {
+  List: params => { return axios.get(url.store, { params: params }).then(res => res.data) }
+}
+
+//Method
+export const requestMethod = {
+  List: params => { return axios.get(url.method, { params: params }).then(res => res.data) }
+}
+
+//Cart
+export const requestCart = {
+  List: params => { return axios.get(url.cart, { params: params }).then(res => res.data) },
+  Create: params => { return axios.post(url.cart, params).then(res => res.data) },
+  Update: params => { return axios.patch(url.cart, params).then(res => res.data) }
+}
---
+++ frontend/src/urlConfig.js
@@ -0,0 +1,8 @@
+// Product
+export const product = '/product'
+//Store
+export const store = '/store'
+//Method
+export const method = '/method'
+//cart
+export const cart = '/cart'
