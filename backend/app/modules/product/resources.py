30f2f425c80db565fb95d51fd56e266a379fc9ec
WalkerHo <uitk23009@gmail.com>
2018-02-20 10:51:35 +0800

    Add backend category GET api

--- backend/app/modules/product/resources.py
+++ backend/app/modules/product/resources.py
@@ -2,12 +2,15 @@ from flask import current_app
 from flask_restplus import Namespace, Resource, reqparse
 
 from app.modules.product.models import Category
-from .schemas import ProductSchema
+from .schemas import ProductSchema, CategorySchema
 
 ns = Namespace('product')
 
 product_argparser = reqparse.RequestParser()
 product_argparser.add_argument('id', type=int, store_missing=False)
+
+category_argparse = reqparse.RequestParser()
+category_argparse.add_argument('id', type=int, store_missing=False)
 
 
 @ns.route('')
@@ -22,3 +25,14 @@ class ProductApi(Resource):
             return product_schema.dump(cat.products)
 
         return 'Product GET'
+
+@ns.route('/category')
+class CategoryApi(Resource):
+
+    def get(self):
+        args = category_argparse.parse_args()
+        current_app.logger.debug('Category GET request: {}'.format(args))
+        category_obj = Category.list()
+        category_schema = CategorySchema(many=True)
+        return category_schema.dump(category_obj)
+
--- backend/app/modules/product/schemas.py
+++ backend/app/modules/product/schemas.py
@@ -1,8 +1,14 @@
 from app.extensions import ma
 
-from .models import Product
+from .models import Product, Category
 
 
 class ProductSchema(ma.ModelSchema):
     class Meta:
         model = Product
+
+
+class CategorySchema(ma.ModelSchema):
+    class Meta:
+        model = Category
+        exclude = ('products', )
